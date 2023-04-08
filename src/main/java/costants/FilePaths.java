fa900cb11913a61a149514b23ff5ffa282726f3f
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2023-04-08 12:30:10 +0200

    TESI-8 -> TESI-5: Test Random Label Flipping
    https://gabrielezarcone.atlassian.net/browse/TESI-5

--- src/main/java/costants/FilePaths.java
+++ src/main/java/costants/FilePaths.java
@@ -1,16 +1,20 @@
 package costants;
 
 public class FilePaths {
-    public static final String IRIS_FILE_PATH = "src/main/resources/iris.arff";
 
+    public static final String IRIS_FILE_PATH = "src/main/resources/iris.arff";
     public static final String OUTPUT_FOLDER = "src/main/resources/output/";
 
     public static class ExplorerResults{
-
+        private ExplorerResults(){}
         public static final String PROVA_1 = "src/main/resources/ExplorerResults/original/prova1.arff";
         public static final String PROVA_2 = "src/main/resources/ExplorerResults/original/prova2.arff";
         public static final String PROVA_3 = "src/main/resources/ExplorerResults/original/prova3.arff";
         public static final String PROVA_4 = "src/main/resources/ExplorerResults/original/prova4-3alg.arff";
+    }
 
+    public static class TestPath{
+        private TestPath(){}
+        public static final String IRIS_FILE_PATH = "src/main/resources/test/iris.arff";
     }
 }
