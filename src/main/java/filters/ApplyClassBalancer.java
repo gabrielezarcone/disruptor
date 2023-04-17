f2dc117b0b885f4861a0cdd062e8c372aa4b23df
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2023-04-17 22:37:12 +0200

    TESI-8 -> TESI-5: Add filters for class balancing
    https://gabrielezarcone.atlassian.net/browse/TESI-5

---
+++ src/main/java/filters/ApplyClassBalancer.java
@@ -0,0 +1,39 @@
+package filters;
+
+import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.supervised.instance.ClassBalancer;
+import weka.filters.supervised.instance.SMOTE;
+
+public class ApplyClassBalancer {
+
+
+    /**
+     * Apply the filter ClassBalancer to balance the classes
+     *
+     * @param instances instances to balance
+     * @return the balanced instances
+     * @throws Exception if problems during the execution of the filter
+     */
+    public static Instances classBalancer(Instances instances) throws Exception {
+        ClassBalancer classBalancer = new ClassBalancer();
+        classBalancer.setInputFormat(instances);
+
+        return Filter.useFilter(instances, classBalancer);
+    }
+
+
+    /**
+     * Apply the filter SMOTE to balance the classes
+     *
+     * @param instances instances to balance
+     * @return the balanced instances
+     * @throws Exception if problems during the execution of the filter
+     */
+    public static Instances smote(Instances instances) throws Exception {
+        SMOTE smote = new SMOTE();
+        smote.setInputFormat(instances);
+
+        return Filter.useFilter(instances, smote);
+    }
+}
