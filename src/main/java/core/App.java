93c3576d2ae857110852eb9052f8a7fdddb64078
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2023-04-25 11:27:12 +0200

    TESI-14 -> TESI-20: Remove precedent main logic
    https://gabrielezarcone.atlassian.net/browse/TESI-20

--- src/main/java/core/App.java
+++ src/main/java/core/App.java
@@ -1,58 +1,10 @@
 package core;
 
-import util.ArffUtil;
-import util.DistanceMatrixUtil;
-import costants.FilePaths;
-import weka.core.*;
-import weka.core.matrix.Matrix;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Date;
 
 public class App {
 
 
-    public static void main(String[] args)  {
-        //distanceMatrices();
-        mergeExperimenterArffResults();
-    }
-
-    private static void mergeExperimenterArffResults() {
-        try {
-            Instances mergedInstances = ArffUtil.mergeArffFiles(FilePaths.ExplorerResults.PROVA_1, FilePaths.ExplorerResults.PROVA_4);
-            ArffUtil.exportToArff(mergedInstances, "merged-"+new Date());
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("IO exception");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void distanceMatrices() {
-        try {
-
-            Instances dataset = ArffUtil.readArffFile(FilePaths.IRIS_FILE_PATH, "class");
-
-            printMatrix(dataset, new EuclideanDistance(dataset));
-            printMatrix(dataset, new ManhattanDistance(dataset));
-            printMatrix(dataset, new ChebyshevDistance(dataset));
-            printMatrix(dataset, new MinkowskiDistance(dataset));
-
-        } catch (FileNotFoundException e){
-            e.printStackTrace();
-            System.out.println("File not found");
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("IO exception");
-        }
-    }
-
-    private static void printMatrix(Instances instances, DistanceFunction distanceFunction){
-        DistanceMatrixUtil distanceMatrixUtil = new DistanceMatrixUtil();
-        Matrix matrix = distanceMatrixUtil.computeDistanceMatrix(instances, distanceFunction);
-        System.out.println(matrix);
+    public static void main(String[] args){
     }
 
 }
